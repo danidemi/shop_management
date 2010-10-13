66ed08d3f26e32f5bc9e9c712eead8c3f56ca369
danidemi <demichelis@danidemi.com>
2010-10-13 23:48:36 +0200

    First example of validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,10 @@ class Customer < ActiveRecord::Base
 	belongs_to :company 	# foreign key: company_id
 	has_many :meetings, :dependent => :delete_all
 
+  # Validation
+  validates_presence_of :firstName, :lastName
+  #validates_format_of :email, :with => /\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/
+
   # Pagination
   cattr_reader :per_page
 
