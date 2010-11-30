5c3fe32ebc397d63ee79b1148fdd7f9b15cb98de
danidemi <demichelis@danidemi.com>
2010-11-30 23:14:43 +0100

    >Added colorful flash messages >Fix layout with messages

--- app/controllers/operator_sessions_controller.rb
+++ app/controllers/operator_sessions_controller.rb
@@ -7,17 +7,19 @@ class OperatorSessionsController < ApplicationController
  def create  
    @operator_session = OperatorSession.new(params[:operator_session])  
    if @operator_session.save  
-     flash[:notice] = "Successfully logged in."  
+     flash[:notice] = t('login.message.success')
      redirect_to :controller => :welcome, :action => :index
    else  
-     render :action => 'new'  
+      #render :action => 'new'  
+      flash[:warning] = t('login.message.failed')
+      redirect_to :controller => :welcome, :action => :index
    end  
  end 
 
  def destroy  
    @operator_session = OperatorSession.find  
    @operator_session.destroy  
-   flash[:notice] = "Successfully logged out."  
+   flash[:notice] = t('login.message.logout')
    redirect_to root_url  
  end
 
