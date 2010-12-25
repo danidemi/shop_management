2f5cd9110806b11dcb941639090cba7e1adf8656
danidemi <demichelis@danidemi.com>
2010-12-25 11:19:28 +0100

    Jquery and css added. Updated icons. Nice effect on item pages.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var $j = jQuery.noConflict();
+
+$j(document).ready(function(){
+
+$j("table.items tbody tr").hover(
+  function(){
+    $j(this).addClass("hovering");
+    $j(this).find('div.controls').show();
+  },
+  function(){
+    $j(this).removeClass("hovering");
+    $j(this).find('div.controls').hide();
+  }
+);
+
+});
